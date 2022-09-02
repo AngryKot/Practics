3cb039c47b8814be14cbf6311c33144e079eefe0
Angry Kot <segeda.nikita2016@yandex.ru>
2022-09-02 12:57:01 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Regular_expr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст\n");
+            string s = Console.ReadLine();
+            Regex regex = new Regex(@"8[\(\s-]*[0-9]{3}[)\s-][0-9]{3}-[0-9]{2}-[0-9]{2}");
+            MatchCollection matches = regex.Matches(s);
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                    Console.WriteLine(match.Value);
+            }
+            else
+            {
+                Console.WriteLine("Совпадений не найдено");
+            }
+        }
+    }
+}
